0b8fd53781bbab9c99f6fd2a636ac8d302b3b85a
fapinter <fpinterich@hotmail.com>
2024-09-20 10:19:43 -0300

    Starting ListNode, List, still need to test it

--- List.java
+++ List.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+
 public class List {
+    ListNode head;
+
+    public List(int numNodes, ArrayList<String> fileNames){
+        this.head = null;
+        ListNode prev = null;
+        for(int i = 0; i < numNodes; i++){
+            ListNode node = new ListNode(fileNames.get(i), null);
+            if(prev != null){
+                prev.setNext(node);
+            }
+            else{
+                this.head = node;
+            }
+            prev = node;
+        }
+    }
+    public boolean vazia(){
+        boolean bool = false;
+        if(this.head == null){
+            bool = true;
+        }
+        return bool;
+    }
+    public void addNodeFrequency(String fileName){
+        boolean inserted = false;
+        ListNode curr = head;
+        while(!inserted){
+            if(curr.getFile_name().equals(fileName)){
+                curr.increaseFrequency();
+                inserted = true;
+            }
+            else{
+                if(curr.getNext() == null){
+                    System.out.println("ERRO: file node not found");
+                }
+                curr = curr.getNext();
+
+            }
+        }
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,6 @@
 public class Main {
+
+    //Pegar o diretorio, fazer a verificacao
+    //Pegar numero de arquivos e inserir seus nomes em uma Lista
+    //Construtor da Lista ira criar todos os Nodes diretamente
 }
